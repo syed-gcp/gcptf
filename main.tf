c919b9c1626de2636361ae5f9770bda8e611254d
Rubb <syed.rubb@bt.com>
2020-04-09 10:50:04 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = var.machine_types[var.environment]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
+module "storage_bucket" {
+    source = ".//modules//google-static-website-bucket"
+    bucket_name = "syedrubb-070420"
+
+}
+
